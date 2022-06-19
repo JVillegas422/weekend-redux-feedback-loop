c56359abbd7ab03b14a7da4bfaec433478df0e2b
Juan Villegas <jvillegas422@gmail.com>
2022-06-19 14:05:31 -0500

    Added router

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const feedbackRouter = express.Router();
+const pool = require('../modules/pool')
+
+// POST
+router.post('/', (req, res) => {
+    // come back after finishing forms
+})
