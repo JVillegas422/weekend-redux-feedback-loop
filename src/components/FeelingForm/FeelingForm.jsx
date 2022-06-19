dbc2dabc1d2cc7f2be5d7720a4e389be4aae88bc
Juan Villegas <jvillegas422@gmail.com>
2022-06-19 13:39:01 -0500

    Updated feelingForm

--- src/components/FeelingForm/FeelingForm.jsx
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -4,10 +4,34 @@ import { useDispatch } from 'react-redux';
 
 function FeelingForm() {
     
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [feelingForm, setFeelingForm] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        dispatch({
+            type: 'ADD_FEELING_FORM',
+            payload: feelingForm,
+        })
+        history.push('/feelings');
+    };
 
 
     return (
         <>
+            <h3>How are you feeling today?</h3>
+            <form onSubmit={(evt) => handleSubmit(evt)}>
+                <input 
+                    onChange={evt => setFeelingForm(Number(evt.target.value))}
+                    type='range'
+                    min='0'
+                    max='5'
+                    placeholder='Feeling?'
+                    value={feelingForm}
+                />
+                <button type='submit'>Next</button>
+            </form>
         </>
     )
 }
