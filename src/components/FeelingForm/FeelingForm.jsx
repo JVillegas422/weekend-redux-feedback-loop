800a28a840056725f60f20c02017b0d9ea0612f4
Juan Villegas <jvillegas422@gmail.com>
2022-06-19 13:26:41 -0500

    Working on FeelingForm

---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+function FeelingForm() {
+    
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default FeelingForm;
