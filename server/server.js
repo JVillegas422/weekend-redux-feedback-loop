9b66dc4805599860593d2ecff297c02604daa632
Juan Villegas <jvillegas422@gmail.com>
2022-10-29 13:11:13 -0500

    Add route to server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+const feedbackrouter = require('./routes/Feedback.router');
+app.use('/feedback', feedbackrouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
