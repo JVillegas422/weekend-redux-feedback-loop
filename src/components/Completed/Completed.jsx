78b0427c33c6697bdc0253b6e7c3d6466265fa4c
Juan Villegas <jvillegas422@gmail.com>
2022-10-30 15:44:46 -0500

    Add btn effect in Completed

--- src/components/Completed/Completed.jsx
+++ src/components/Completed/Completed.jsx
@@ -1,5 +1,6 @@
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import Header from '../Header/Header';
 
 function Completed() {
     const dispatch = useDispatch();
@@ -15,12 +16,20 @@ function Completed() {
     
     return (
         <>
-            <h2>Thank you for the Feedback!</h2>
+            <Header />
+            <div className='changeFont'>
+                <h2>Thank you for the Feedback!</h2>
+            </div>
             <button
                 onClick={handleSubmit}
                 type="button"
+                className='btn-neon'
             >
-                Leave New Feedback
+                <span>Leave New Feedback</span>
+                <svg height="120" width="210">
+                        <polyline points="0,0 210,0 210,120 0,120 0,00">
+                        </polyline>
+                </svg>
             </button>
         </>
     );
